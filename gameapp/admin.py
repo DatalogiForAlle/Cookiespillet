5185a7d316cee85287b2f0e4cdcbd7525660eb4f
Martin Thoft Jensen <mtj@di.ku.dk>
2022-02-02 15:11:30 +0100

    First database migration

--- gameapp/admin.py
+++ gameapp/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
+from .models import Game, Student, Result
+
 # Register your models here.
+class GameAdmin(admin.ModelAdmin):
+    list_display = (
+        "game",
+        "game_over",
+    )
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = (
+        "game",
+        "name",
+        "score",
+    )
+
+
+class ResultAdmin(admin.ModelAdmin):
+    list_display = ("student",)
+
+
+admin.site.register(Game, GameAdmin)
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Result, ResultAdmin)
--- gameapp/models.py
+++ gameapp/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Game(models.Model):
+    context_object_name = "Game"
+    game = models.CharField(max_length=16, primary_key=True)
+    # creator_id  # Hører til den Lærer/Account som har oprettet spillet.
+    game_over = models.BooleanField(default=False)
+
+    # def check_game_over(self):
+    #    """
+    #    Checks if the game state should be set to game_over.
+    #    """
+    #    if (no time left...):
+    #        return True
+
+    # created_at = models.DateTimeField(auto_now_add=True, null=True)
+    # created_by = models.ForeignKey(
+    #    get_user_model(), null=True, on_delete=models.SET_NULL
+    # )
+
+
+class Student(models.Model):
+    context_object_name = "Student"
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    name = models.CharField(max_length=16)
+    score = models.IntegerField()
+
+    # Nice to have, lærer skal kunne fjerne spillere med upassende navne
+    # removed_from_game = models.BooleanField(default=False)
+
+
+class Result(models.Model):
+    context_object_name = "Results"
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    # cookie_number
+    # point
+    # time
