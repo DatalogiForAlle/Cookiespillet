57ba902f89a79a6894ce0159a69da8b932f1e6d9
Martin Thoft Jensen <mtj@di.ku.dk>
2022-10-15 18:05:28 -0600

    Added checkbox cookie template, not yet implemented in game

---
+++ static/js/checkbox.js
@@ -0,0 +1,30 @@
+var checkbox1 = document.querySelector("input[name=a]");
+var checkbox2 = document.querySelector("input[name=b]");
+var checkbox3 = document.querySelector("input[name=c]");
+var checkbox4 = document.querySelector("input[name=d");
+
+checkbox1.addEventListener('change', function () {
+    if (!checkbox1.checked && !checkbox2.checked && !checkbox3.checked && !checkbox4.checked) {
+        //console.log("NO CHECKED BOXES");
+        document.getElementById("send").value = 0;
+        //console.log(document.getElementById("send").value)
+    }
+});
+
+checkbox2.addEventListener('change', function () {
+    if (!checkbox1.checked && !checkbox2.checked && !checkbox3.checked && !checkbox4.checked) {
+        document.getElementById("send").value = 0;
+    }
+});
+
+checkbox3.addEventListener('change', function () {
+    if (!checkbox1.checked && !checkbox2.checked && !checkbox3.checked && !checkbox4.checked) {
+        document.getElementById("send").value = 0;
+    }
+});
+
+checkbox4.addEventListener('change', function () {
+    if (!checkbox1.checked && !checkbox2.checked && !checkbox3.checked && !checkbox4.checked) {
+        document.getElementById("send").value = 0;
+    }
+});
