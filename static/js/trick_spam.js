9e01d6e6aec8b401ee92f92d8fe0ab8330164eea
Martin Thoft Jensen <mtj@di.ku.dk>
2022-10-16 19:02:36 -0600

    Added trick_spam cookie template, not yet implemented in game.

---
+++ static/js/trick_spam.js
@@ -0,0 +1,14 @@
+function makeAlert() {
+    alert("Vi gerne have at du fortsat bruger cookies!");
+};
+
+makeAlert()
+makeAlert()
+makeAlert()
+makeAlert()
+makeAlert()
+makeAlert()
+makeAlert()
+makeAlert()
+makeAlert()
+makeAlert()
